85b037782e89c716238eeb5789c194e9f0aaa5b5
Dan Conrad <dan.m.conrad@gmail.com>
2016-12-14 22:14:41 -0600

    feat: initial port from cv

---
+++ src/partials/Education.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Education extends Component {
+  render() {
+    return (
+      <section className="Work Education">
+        <h3>Education</h3>
+
+        <div className="job">
+          <div className="position">University of Illinois at Chicago</div>
+          <div className="company">College of Business Administration</div>
+          <div className="date">Aug 2005 – Jun 2010</div>
+        </div>
+      </section>
+
+    );
+  }
+}
+
+export default Education;
