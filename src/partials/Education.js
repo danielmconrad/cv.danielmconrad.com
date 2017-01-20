163c2d32438513c79b46bef1eabf84646c7bb6ce
Dan Conrad <dconrad@groupon.com>
2017-01-20 16:34:12 -0600

    feat: the internet

--- src/partials/Education.js
+++ src/partials/Education.js
@@ -7,6 +7,12 @@ class Education extends Component {
         <h3>Education</h3>
 
         <div className="job">
+          <div className="position">The Internet</div>
+          <div className="company">Giving credit where credit is due.</div>
+          <div className="date">Circa 1997 – Present</div>
+        </div>
+
+        <div className="job">
           <div className="position">University of Illinois at Chicago</div>
           <div className="company">College of Business Administration</div>
           <div className="date">Aug 2005 – Jun 2010</div>
